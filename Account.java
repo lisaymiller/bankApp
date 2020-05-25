4c362e9512d38bcc3a7dcf66bfd2b76787eed761
Lisa Miller <lisayevonne@outlook.com>
2020-05-25 11:15:40 -0400

    csv

--- Account.java
+++ Account.java
@@ -30,7 +30,31 @@ public abstract class Account implements IBaseRate {
         return lastTwoSsn + uniqueID + randNum;
     }
 
+    public void compound() {
+        double accruedInterest = balance * (rate / 100);
+        System.out.println("Accrued interest: $" + accruedInterest);
+    }
+
     // list common methods
+    public void deposit(double amount) {
+        balance = balance + amount;
+        System.out.println("Depositing $" + amount);
+    }
+
+    public void withdrawl(double amount) {
+        balance = balance - amount;
+        System.out.println("Withdrawing $" + amount);
+    }
+
+    public void transfer(String toWhere, double amount) {
+        balance = balance - amount;
+        System.out.println("Transferring $" + amount + " to " + toWhere);
+    }
+
+    public void printBalance() {
+        System.out.println("Your balance is now $" + amount);
+    }
+
     public void showInfo() {
         System.out.println("NAME: " + name + "\nACCOUNT NUMBER: " + acctNum + "\nACCOUNT BALANCE: " + balance
                 + "\nRATE: " + rate + " %");
